dcf98ee0e1593dc6d5c39cd04e7bd2e9c2e3e18f
Bohdan Chornopolskyi <bohdan.chornopolskyi@gmail.com>
2025-02-11 17:57:21 +0100

    fix package for confetti, fix eslint error

--- src/components/confetti.tsx
+++ src/components/confetti.tsx
@@ -1,16 +1,20 @@
 'use client';
 
+/* eslint-disable @next/next/no-img-element */
+
 import { useEffect } from 'react';
 import confetti from 'canvas-confetti';
 
 export default function Confetti() {
   useEffect(() => {
+    // eslint-disable-next-line
     confetti({
       particleCount: 100,
       spread: 140,
       origin: { y: 0.5 },
     });
     const interval = setInterval(() => {
+      // eslint-disable-next-line
       confetti({
         particleCount: 100,
         spread: 140,
